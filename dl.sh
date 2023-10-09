561912e1df8c8ba8d056cc77c14ef151fd63bd40
billmills <you@example.com>
2023-10-09 14:10:20 -0400

    checkpoint

--- dl.sh
+++ dl.sh
@@ -1,53 +1,51 @@
-YEAR=1993
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
-sleep 900
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
-sleep 900
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
-sleep 900
-YEAR=1994
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
-sleep 900
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
-sleep 900
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
-sleep 900
-YEAR=1995
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
-sleep 900
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
-sleep 900
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
-sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# YEAR=1993
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
 # sleep 900
-
-
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# sleep 900
+# YEAR=1994
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# sleep 900
+# YEAR=1995
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# sleep 900
 # YEAR=2000
 # sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
 # sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
@@ -193,6 +191,166 @@ sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
 # sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
 # sleep 900
 # YEAR=2009
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# sleep 900
+# YEAR=2010
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# sleep 900
+# YEAR=2011
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# sleep 900
+# YEAR=2012
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# sleep 900
+# YEAR=2013
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# sleep 900
+# YEAR=2014
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# sleep 900
+# YEAR=2015
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# sleep 900
+# YEAR=2016
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# sleep 900
+# YEAR=2017
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# sleep 900
+# YEAR=2018
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|04|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|05|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|06|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|07|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|08|g' | kubectl apply -f -
+# sleep 900
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|09|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|10|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|11|g' | kubectl apply -f -
+# sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|12|g' | kubectl apply -f -
+# sleep 900
+# YEAR=2019
 # sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|01|g' | kubectl apply -f -
 # sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|02|g' | kubectl apply -f -
 # sed "s|YYYY|${YEAR}|g" pod-template.yaml | sed 's|MM|03|g' | kubectl apply -f -
